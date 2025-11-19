df069d31000e34d4d001c2d7786caaebfcee9129
steve swoyer <steve@flox.dev>
2025-11-19 10:23:13 -0500

    ...
    Signed-off-by: steve swoyer <steve@flox.dev>

--- .flox/pkgs/backlog-md.nix
+++ .flox/pkgs/backlog-md.nix
@@ -1,11 +1,6 @@
-{ callPackage, fetchFromGitHub, lib }:
+{ callPackage, fetchFromGitHub }:
 let
   upstream = import ../lib/fetch-upstream.nix { inherit fetchFromGitHub; };
-  pkg = callPackage "${upstream}/packages/backlog-md/package.nix" { };
 in
-# Override platform restriction - upstream only allows x86_64-linux but it should work elsewhere
-pkg.overrideAttrs (oldAttrs: {
-  meta = oldAttrs.meta // {
-    platforms = lib.platforms.linux;  # Support all Linux platforms, not just x86_64
-  };
-})
+# Note: This package only supports x86_64-linux due to architecture-specific dependencies
+callPackage "${upstream}/packages/backlog-md/package.nix" { }
