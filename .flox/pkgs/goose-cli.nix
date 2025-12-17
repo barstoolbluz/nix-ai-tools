76971f72808c48715c909943a5d62b85924c4978
steve swoyer <steve@flox.dev>
2025-12-16 22:33:57 -0500

    ...
    Signed-off-by: steve swoyer <steve@flox.dev>

--- .flox/pkgs/goose-cli.nix
+++ .flox/pkgs/goose-cli.nix
@@ -7,13 +7,13 @@
   xorg,
 }:
 let
-  version = "1.15.0";
+  version = "1.16.1";
 
   # Map Nix platforms to Goose platform naming
   platformMap = {
     "x86_64-linux" = {
       url = "https://github.com/block/goose/releases/download/v${version}/goose-x86_64-unknown-linux-gnu.tar.bz2";
-      hash = "sha256-V47+E0KduKHkryNDh2Vqx14pTeFl6w1vGs3uiF8Vqwk=";
+      hash = "sha256-rwm0gp9bv7+WGy+Eqn6zH6d98D4RKWqeyh3VVObaxpE=";
     };
     "aarch64-linux" = {
       url = "https://github.com/block/goose/releases/download/v${version}/goose-aarch64-unknown-linux-gnu.tar.bz2";
