5d2012cb919b7d0b29ec32a4e1b61a0e052fedc4
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2026-04-30 02:28:50 -0400

    goose-cli: 1.32.0 -> 1.33.1 (#80)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .flox/pkgs/goose-cli.nix
+++ .flox/pkgs/goose-cli.nix
@@ -7,25 +7,25 @@
   xorg,
 }:
 let
-  version = "1.32.0";
+  version = "1.33.1";
 
   # Map Nix platforms to Goose platform naming
   platformMap = {
     "x86_64-linux" = {
       url = "https://github.com/block/goose/releases/download/v${version}/goose-x86_64-unknown-linux-gnu.tar.bz2";
-      hash = "sha256-IPCccBftdys7I09zLE0qYrZZ86ZzsAZ2BRvRD4GMBqk=";
+      hash = "sha256-5dyaDxix6Y5fPuLofaHqVy8LphBTsP+Lo+y5KVcJebw=";
     };
     "aarch64-linux" = {
       url = "https://github.com/block/goose/releases/download/v${version}/goose-aarch64-unknown-linux-gnu.tar.bz2";
-      hash = "sha256-wCTlSU166egFCw7yK9DpJ6PZHRkp+t17ffAzokTer5Y=";
+      hash = "sha256-D64WXFZL7bk087NYjjTbawwwAuHjDnNn1j8UYp73vPA=";
     };
     "x86_64-darwin" = {
       url = "https://github.com/block/goose/releases/download/v${version}/goose-x86_64-apple-darwin.tar.bz2";
-      hash = "sha256-NahjvHTqkz0AXpusY/2xcPUBrTVg7uyIVOQHYVhOlkM=";
+      hash = "sha256-vFSWwqHISwmdQVVXiZ2e2izZAkq4Pz3LliQjzTiyRR4=";
     };
     "aarch64-darwin" = {
       url = "https://github.com/block/goose/releases/download/v${version}/goose-aarch64-apple-darwin.tar.bz2";
-      hash = "sha256-kXrIqxrpodY7OyeFzMQsFx9u+XwcpER6++dpTnqabwA=";
+      hash = "sha256-jAQxSwsZnYadkv4y+q82ghlQcZQi5CILHq/IeFhM6Vo=";
     };
   };
 
