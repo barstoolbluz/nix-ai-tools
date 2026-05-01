17b6111c1da7776ed5ea97bfd09aabc4b63e83be
sswoyer <54217314+barstoolbluz@users.noreply.github.com>
2026-05-01 02:37:56 -0400

    upstream: de2a3af8 -> 285bc277 (#84)
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .flox/pkgs/fetch-upstream.nix
+++ .flox/pkgs/fetch-upstream.nix
@@ -5,6 +5,6 @@
 fetchFromGitHub {
   owner = "numtide";
   repo = "llm-agents.nix";
-  rev = "de2a3af876b071dfc43afa14976edfc89fd585b4";
-  hash = "sha256-xoe/d6DI99r3MWlbS1+3A82NnD6uMpdgEQNqn7cp7Y0=";
+  rev = "285bc2776013c9645f0fe08f8904f6dd13a0e606";
+  hash = "sha256-xJ+yhTIbx5ZHRTXnM9dNj2Ont7ewqU9SAIbDrd7U8X8=";
 }
